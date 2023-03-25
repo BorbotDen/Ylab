acb94a0abf7e45208f7359711915d32dd8d38c4d
Denis Borbot <Borbot@mail.ru>
2023-03-25 17:11:32 +0400

    Tasks4 start

---
+++ Tasks/ylab-lesson4-template/src/main/java/io/ylab/intensive/lesson04/eventsourcing/api/PersonApi.java
@@ -0,0 +1,15 @@
+package main.java.io.ylab.intensive.lesson04.eventsourcing.api;
+
+import java.util.List;
+
+import main.java.io.ylab.intensive.lesson04.eventsourcing.Person;
+
+public interface PersonApi {
+  void deletePerson(Long personId);
+
+  void savePerson(Long personId, String firstName, String lastName, String middleName);
+
+  Person findPerson(Long personId);
+
+  List<Person> findAll();
+}
---
+++ Tasks/ylab-lesson4-template/src/main/java/io/ylab/intensive/lesson04/movie/MovieLoader.java
@@ -0,0 +1,7 @@
+package main.java.io.ylab.intensive.lesson04.movie;
+
+import java.io.File;
+
+public interface MovieLoader {
+  void loadData(File file);
+}
